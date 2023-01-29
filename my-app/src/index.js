a3eb91477688a44f4d4e399d49d66735d2397b5b
Arturo-Perevoznyk <arturo.ipv@protonmail.com>
2023-01-29 15:14:53 +0000

    #

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Button } from './App.js'
-import { Circle } from './App.js'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <h1>Damn! What a button!</h1>
-    <p>Clicked: x times</p>
-    <Button text='Press to change color'/>
-    <Circle/>
-    <Circle/>
-  </>
-);
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);  
