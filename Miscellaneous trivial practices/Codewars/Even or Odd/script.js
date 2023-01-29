8ec699b45f8834b0a58af65b2fe6f9f2d06f2d74
Arturo-Perevoznyk <arturo.ipv@protonmail.com>
2023-01-29 15:14:09 +0000

    #

---
+++ Miscellaneous trivial practices/Codewars/Even or Odd/script.js
@@ -0,0 +1,9 @@
+function positiveSum(arr) {
+    const initial = 0;
+    arr.reduce((previ, current) => {
+        current > 0 ? previ + current initial
+    }, initial
+        )
+  }
+
+console.log(getCount('Pepe perez se va de viaje'))
