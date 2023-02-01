640fb240ac1adf5621508b87b4c80984c9f18e14
Arturo-Perevoznyk <arturo.ipv@gmail.com>
2023-02-01 10:07:37 +0000

    #

--- DOM practice/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('button')
-
-button.addEventListener('click', () => {
-    const newColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`
-    button.style.backgroundColor = newColor;
-})
